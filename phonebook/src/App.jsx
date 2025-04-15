fc1a21d1bcd7a7b79bc6351076098e1722752c4a
henrique-lh <henrique.santos.lhls@gmail.com>
2025-04-15 20:08:41 -0300

    - update

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from 'react';
+import Filter from "./components/Filter.jsx";
+import PersonForm from "./components/PersonForm.jsx";
+import Persons from "./components/Persons.jsx";
+import contactService from "./service/contact.js";
+import Notification from "./components/Notification.jsx";
+
+const App = () => {
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newPhone, setNewPhone] = useState('')
+    const [_lastId, setLastId] = useState(0)
+    const [showPersons, setShowPersons] = useState('')
+    const [notificationMessage, setNotificationMessage] = useState(null)
+    const [colorMessage, setColorMessage] = useState('')
+
+    useEffect(() => {
+        contactService
+            .read()
+            .then(initialContacts => {
+                setPersons(initialContacts)
+                setLastId(initialContacts.length)
+            })
+    }, [])
+
+    const addName = (event) => {
+        event.preventDefault()
+        const newId = persons.length + 1
+        setLastId(newId)
+        const contactObject = {
+            name: newName,
+            number: newPhone,
+            id: newId.toString()
+        }
+        const exists = persons.find(p => p.name === newName)
+        if (newPhone.length === 0) {
+            window.alert("Please enter a phone")
+        }
+        else if (newName.length === 0) {
+            window.alert("Please enter a name")
+        }
+        else if (exists) {
+            if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+                const existingContactWithUpdatedNumber = {...exists, number: newPhone}
+                contactService
+                    .updateContact(existingContactWithUpdatedNumber.id, existingContactWithUpdatedNumber)
+                    .then(returnedContact => {
+                        setPersons(persons.map(p => p.id === returnedContact.id ? returnedContact : p))
+                        setNotificationMessage(
+                            `${newName} phone's number updated`
+                        )
+                        setColorMessage('green')
+                        setNewName('')
+                        setNewPhone('')
+                        setTimeout(() => {
+                            setNotificationMessage(null)
+                            setColorMessage('')
+                        }, 2000)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        setNotificationMessage(
+                            `Information about ${newName} has already been removed from server`
+                        )
+                        setColorMessage('red')
+                        setPersons(persons.filter(p => p.id !== exists.id))
+                        setTimeout(() => {
+                            setNotificationMessage(null)
+                            setColorMessage('')
+                        }, 3000)
+                    })
+            }
+        }
+        else {
+
+            contactService
+                .create(contactObject)
+                .then(returnedContacts => {
+                    setPersons(persons.concat(returnedContacts))
+                    setNotificationMessage(
+                        `Added ${newName}`
+                    )
+                    setColorMessage('green')
+                    setNewName('')
+                    setNewPhone('')
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                        setColorMessage('')
+                    }, 2000)
+                })
+        }
+    }
+
+    const deleteContact = (id) => {
+        contactService
+            .deleteContact(id)
+            .then(returnedData => {
+                setPersons(persons.filter(p => p.id !== returnedData.id))
+            })
+    }
+
+    const handleShowedPersons = (event) => {
+        const name = event.target.value
+        setShowPersons(name)
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handlePhoneChange = (event) => {
+        setNewPhone(event.target.value)
+    }
+
+    const personsToShow = showPersons.length === 0
+        ? persons
+        : persons.filter(p => p.name.includes(showPersons))
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+
+            <Notification message={notificationMessage} colorMessage={colorMessage} />
+
+            <Filter showPersons={showPersons} handleShowedPersons={handleShowedPersons} />
+
+            <h3>add a new</h3>
+
+            <PersonForm
+                addName={addName}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newPhone={newPhone}
+                handlePhoneChange={handlePhoneChange}
+            />
+
+            <h2>Numbers</h2>
+
+            <Persons persons={personsToShow} deleteContact={deleteContact} />
+        </div>
+    )
+}
+
+export default App
