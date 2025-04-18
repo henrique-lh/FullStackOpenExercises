2f7ec7baf506beb03a6697846439a156d1754496
henrique-lh <henrique.santos.lhls@gmail.com>
2025-04-18 18:50:44 -0300

    - exercises 3.5 - exercise 3.6

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 let persons = [
     {
@@ -55,6 +56,38 @@ app.delete('/api/persons/:id', (request, response) => {
         persons = persons.filter(person => person.id !== personId)
         response.status(204).send(`Deleted ${person.name} from phonebook`)
     }
+})
+
+const generateRandomId = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+app.post('/api/persons', (request, response) => {
+    const personId = generateRandomId(5, 1000)
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const foundName = persons.find(p => p.name === body.name)
+    if (foundName) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: String(personId),
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
